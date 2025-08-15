d036c1de1652fbdfc576ac671e807cfdb8e57853
rolandsmithrps <31712002+rolandsmithrps@users.noreply.github.com>
2025-08-14 23:24:14 -0700

    Update zoom-config.js

--- zoom-config.js
+++ zoom-config.js
@@ -1,8 +1,8 @@
 /** Edit these if hosting via the portal domain (models.localgirls.io). */
 window.ZOOM_CFG = {
-  SDK_KEY: "YOUR_CLIENT_ID",
-  MEETING_NUMBER: "YOUR_MEETING_NUMBER",
-  PASSCODE: "YOUR_MEETING_PASSCODE"
+  SDK_KEY: "OyW38yPdSFulmfHtyLY45A",
+  MEETING_NUMBER: "6764982991",
+  PASSCODE: "46FDQ8"
 ,
   // Optional: provide a ZAK token to start as true host in browser
   ZAK: ""
