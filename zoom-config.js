71470bad2a9449112fd68e827a511dab4b566abc
rolandsmithrps <31712002+rolandsmithrps@users.noreply.github.com>
2025-08-14 22:55:50 -0700

    Update zoom-config.js

--- zoom-config.js
+++ zoom-config.js
@@ -1,7 +1,7 @@
 
 /** Optional: only needed if you host in-browser on the portal domain. */
 window.ZOOM_CFG = {
-  SDK_KEY: "YOUR_ZOOM_SDK_KEY",
-  MEETING_NUMBER: "YOUR_MEETING_NUMBER",
-  PASSCODE: "YOUR_MEETING_PASSCODE"
+  SDK_KEY: "YOyW38yPdSFulmfHtyLY45A",
+  MEETING_NUMBER: "123456789",
+  PASSCODE: ""
 };
